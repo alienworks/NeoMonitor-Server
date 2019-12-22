b37893dc50c01af3c2ceedfa47ea9f86d33587b5
LeaFrock <petergeorge@126.com>
2019-12-22 20:59:29 +0800

    Change the LogLevel setting

--- NodeMonitor/Services/NotificationService.cs
+++ NodeMonitor/Services/NotificationService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NeoMonitor.Data.Models;
 using NodeMonitor.Hubs;
 using NodeMonitor.Infrastructure;
@@ -15,26 +17,25 @@ namespace NodeMonitor.Services
 	{
 		private readonly NodeSynchronizer _nodeSynchronizer;
 		private readonly IHubContext<NodeHub> _nodeHub;
+		private readonly ILogger<NotificationService> _logger;
 
-		public NotificationService(NodeSynchronizer nodeSynchronizer, IHubContext<NodeHub> nodeHub)
+		public NotificationService(NodeSynchronizer nodeSynchronizer, IHubContext<NodeHub> nodeHub, ILogger<NotificationService> logger)
 		{
 			_nodeSynchronizer = nodeSynchronizer;
 			_nodeHub = nodeHub;
+			_logger = logger;
 		}
 
 		protected override async Task ExecuteAsync(CancellationToken cancelToken)
 		{
+			Stopwatch sw = new Stopwatch();
 			while (!cancelToken.IsCancellationRequested)
 			{
-				Console.WriteLine("Syncing... ...");
-				try
-				{
-					await UpdateBlockCountAsync();
-				}
-				catch (Exception)
-				{
-					throw;
-				}
+				_logger.LogInformation("[{0}] Syncing... ...", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+				sw.Restart();
+				await UpdateBlockCountAsync();
+				sw.Stop();
+				_logger.LogInformation("[{0}] UpdateBlockCountAsync: {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), sw.Elapsed.ToString());
 				await Task.Delay(5000, cancelToken);
 			}
 		}
