2bd34c9ea89181bb0dd87a1aae368d5f234e5cee
LeaFrock <petergeorge@126.com>
2019-12-22 21:00:48 +0800

    Use List<T> instead of IEnumerable<T>

--- NeoState.Common/RPC/RPCPeersResponse.cs
+++ NeoState.Common/RPC/RPCPeersResponse.cs
@@ -6,12 +6,12 @@ namespace NeoState.Common.RPC
 	public class RPCPeersResponse
 	{
 		[JsonProperty(PropertyName = "unconnected")]
-		public IEnumerable<RPCPeer> Unconnected { get; set; }
+		public List<RPCPeer> Unconnected { get; set; }
 
 		[JsonProperty(PropertyName = "bad")]
-		public IEnumerable<RPCPeer> Bad { get; set; }
+		public List<RPCPeer> Bad { get; set; }
 
 		[JsonProperty(PropertyName = "connected")]
-		public IEnumerable<RPCPeer> Connected { get; set; }
+		public List<RPCPeer> Connected { get; set; }
 	}
 }
