1e3e648ae132d552f5b72f35a842e1505d07eed3
LeaFrock <petergeorge@126.com>
2020-01-26 20:02:41 +0800

    Preparing for backend analysis module.

---
+++ NeoMonitor.Analysis.Web/Models/IpVisitAnaData.cs
@@ -0,0 +1,15 @@
+﻿namespace NeoMonitor.Analysis.Web.Models
+{
+    public sealed class IpVisitAnaData
+    {
+        public string Ip { get; set; }
+
+        public int Times { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+        public int Day { get; set; }
+    }
+}
---
+++ NodeMonitor/Controllers/AnalysisController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NodeMonitor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnalysisController : ControllerBase
+    {
+        [HttpGet("totalVisitTimes")]
+        public ActionResult<int> GetTotalVisitTimes()
+        {
+            return 1;
+        }
+
+        [HttpGet("totalIpCount")]
+        public ActionResult<int> GetTotalIpCount()
+        {
+            return 1;
+        }
+    }
+}
