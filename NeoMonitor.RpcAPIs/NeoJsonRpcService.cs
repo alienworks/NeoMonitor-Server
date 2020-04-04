b45c9c2892045bd15e12b2f0e696f1dd276cab60
LeaFrock <petergeorge@126.com>
2020-04-05 00:39:19 +0800

    Set default id as 1

--- NeoMonitor.RpcAPIs/NeoJsonRpcService.cs
+++ NeoMonitor.RpcAPIs/NeoJsonRpcService.cs
@@ -15,25 +15,25 @@ namespace NeoMonitor.RpcAPIs
             _rpcHttpClient = rpcHttpClient;
         }
 
-        public async Task<int?> GetBlockCountAsync(string url, long id)
+        public async Task<int?> GetBlockCountAsync(string url, long id = 1)
         {
             var rsp = await _rpcHttpClient.PostAsync<int?>(url, new RpcRequestBody("getblockcount") { Id = id });
             return rsp.Success ? rsp.Body.Result : default;
         }
 
-        public async Task<List<string>> GetRawMemPoolAsync(string url, long id)
+        public async Task<List<string>> GetRawMemPoolAsync(string url, long id = 1)
         {
             var rsp = await _rpcHttpClient.PostAsync<List<string>>(url, new RpcRequestBody("getrawmempool") { Id = id });
             return rsp.Success ? rsp.Body.Result : default;
         }
 
-        public async Task<PeerModel> GetPeersAsync(string url, long id)
+        public async Task<PeerModel> GetPeersAsync(string url, long id = 1)
         {
             var rsp = await _rpcHttpClient.PostAsync<PeerModel>(url, new RpcRequestBody("getpeers") { Id = id });
             return rsp.Success ? rsp.Body.Result : default;
         }
 
-        public async Task<VersionModel> GetVersionAsync(string url, long id)
+        public async Task<VersionModel> GetVersionAsync(string url, long id = 1)
         {
             var rsp = await _rpcHttpClient.PostAsync<VersionModel>(url, new RpcRequestBody("getversion") { Id = id });
             return rsp.Success ? rsp.Body.Result : default;
