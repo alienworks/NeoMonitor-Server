552d63a7a4bc3470e9ed43475a155dcc4588c6dd
LeaFrock <petergeorge@126.com>
2020-04-09 01:14:05 +0800

    Add NodeSyncSettings

---
+++ NeoMonitor/Configs/NodeSyncSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace NeoMonitor.Configs
+{
+    public sealed class NodeSyncSettings
+    {
+        public int ExceptionFilter { get; set; }
+
+        public int ParallelDegree { get; set; } = 100;
+    }
+}
