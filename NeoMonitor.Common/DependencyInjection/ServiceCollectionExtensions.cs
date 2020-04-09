4be4fa89f3176483c7690a823e4d48560dabd4a9
LeaFrock <petergeorge@126.com>
2020-04-09 23:12:13 +0800

    Bug fix: gzipped response not decompressed

--- NeoMonitor.Common/DependencyInjection/ServiceCollectionExtensions.cs
+++ NeoMonitor.Common/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 using NeoMonitor.Common.IP;
 using NeoMonitor.Common.IP.Configs;
 using NeoMonitor.Common.IP.Services;
@@ -11,7 +14,18 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             services
                 .Configure<IpStackSettings>(config.GetSection(nameof(IpStackSettings)))
-                .AddHttpClient<ILocateIpService, IpStackService>();
+                .AddHttpClient<ILocateIpService, IpStackService>(client =>
+                {
+                    client.BaseAddress = new Uri("http://api.ipstack.com/");
+                    client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+                    client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate");
+                    client.DefaultRequestHeaders.Add("Accept-Language", "zh-CN,zh;q=0.9");
+                    client.DefaultRequestHeaders.Add("DNT", "1");
+                    client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+                    client.DefaultRequestHeaders.Host = "api.ipstack.com";
+                    client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36");
+                })
+                .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler() { AutomaticDecompression = DecompressionMethods.GZip });
             return services;
         }
     }
--- NeoMonitor.Common/IP/Services/IpStackService.cs
+++ NeoMonitor.Common/IP/Services/IpStackService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -17,14 +16,6 @@ namespace NeoMonitor.Common.IP.Services
 
         public IpStackService(HttpClient client, IOptions<IpStackSettings> settings)
         {
-            client.BaseAddress = new Uri("http://api.ipstack.com/");
-            client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate");
-            client.DefaultRequestHeaders.Add("Accept-Language", "zh-CN,zh;q=0.9");
-            client.DefaultRequestHeaders.Add("DNT", "1");
-            client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
-            client.DefaultRequestHeaders.Host = "api.ipstack.com";
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36");
             _httpClient = client;
             _settings = settings.Value;
         }
@@ -32,7 +23,7 @@ namespace NeoMonitor.Common.IP.Services
         public async Task<IpCheckModel> GetLocationAsync(string ip)
         {
             string accessKey = _settings.AccessKey;
-            StringBuilder sb = new StringBuilder(ip.Length + accessKey + 13);
+            StringBuilder sb = new StringBuilder(ip.Length + accessKey.Length + 13);
             sb.Append('/');
             sb.Append(ip);
             sb.Append("?access_key=");
